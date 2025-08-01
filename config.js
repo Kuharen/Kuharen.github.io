695d47150cabd9d527bf657550cb34bd52dd027a
Kuharen <kuharenkoa39@gmail.com>
2025-08-01 22:05:07 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js — финальные рабочие ссылки
 export const SHEET_URLS = {
   men: "https://opensheet.elk.sh/13TgVvLI8Dnfhn40fD_sQarLVuSsMd5_-oma2mgg5Ylc/Лист1",
   women: "https://opensheet.elk.sh/12SCGsWfpTbkKt5htyrPfdUi7hbvK2I7anSvvNs_qy3k/Лист1"
---
+++ start_server.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Запускаем локальный сервер на http://localhost:8000
+python -m http.server 8000
+pause
